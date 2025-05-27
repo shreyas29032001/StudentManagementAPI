dfd9162b663e9ee2f1c619a5b35a80fc9273d03c
Shreyas Khandare <120813211+shreyas29032001@users.noreply.github.com>
2025-05-28 00:27:19 +0530

    Update Student.java

--- StudentManagementApI/src/main/java/com/example/StudentManagementApI/Model/Student.java
+++ StudentManagementApI/src/main/java/com/example/StudentManagementApI/Model/Student.java
@@ -5,49 +5,57 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="studentrecords")
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    
+    private Long id;
     private String name;
     private String email;
     private String course;
+
     public Student() {
     }
-    public Student(long id, String name, String email, String course) {
-        this.id = id;
+
+    public Student(String name, String email, String course) {
         this.name = name;
         this.email = email;
         this.course = course;
     }
-    public long getId() {
+
+    public Long getId() {
         return id;
     }
-    public void setId(long id) {
+
+    public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getCourse() {
         return course;
     }
+
     public void setCourse(String course) {
         this.course = course;
     }
-    
-
 
 }
