5587c96896042fe292a4fdd31c8454b96d72e06b
Shreyas Khandare <120813211+shreyas29032001@users.noreply.github.com>
2025-05-28 00:28:16 +0530

    Update StudentService.java

--- StudentManagementApI/src/main/java/com/example/StudentManagementApI/Service/StudentService.java
+++ StudentManagementApI/src/main/java/com/example/StudentManagementApI/Service/StudentService.java
@@ -1,7 +1,7 @@
 package com.example.StudentManagementApI.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,27 +13,41 @@ public class StudentService {
     @Autowired
     private StudentRepository repository;
 
-    public Student save(Student student){
-        return repository.save(student);
+    public void save(String name,String email,String course){
+        Student user= new Student(name,email,course);
+        repository.save(user);
+        System.out.println("Name: "+name+" Email: "+email+" Course: "+course);
     }
-    public List<Student> getAll() {
+    public List<Student> getStudent(){
         return repository.findAll();
     }
 
-    public Optional<Student> getById(Long id) {
-        return repository.findById(id);
+    public Student getStudentById(Long id) {
+        return repository.findById(id).orElse(null);
     }
 
-    public Student update(Long id, Student updatedStudent) {
-        Student student = repository.findById(id).orElseThrow();
-        student.setName(updatedStudent.getName());
-        student.setEmail(updatedStudent.getEmail());
-        student.setCourse(updatedStudent.getCourse());
-        return repository.save(student);
+    public String updateStudentById(Long id, Student updatedStudent){
+        Student find= repository.findById(id).orElse(null);
+        if(find!=null){
+          find.setName(updatedStudent.getName());
+          find.setEmail(updatedStudent.getEmail());
+          find.setCourse(updatedStudent.getCourse());
+          repository.save(find);
+          return "Exiting student updated";
+        }else{
+            return "Student not Found";
+        }
     }
 
-    public void delete(Long id) {
-        repository.deleteById(id);
+    public String deleteBid(Long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+            return "Deleted successfully";
+        }else{
+            return "Records not Found";
+        }
+        
+
     }
     
 }
